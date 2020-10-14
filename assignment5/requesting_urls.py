1e6aacd0add17dccf75e3c5d1081c319684e7999
Johan Carls <carls@eduroam-193-157-165-126.wlan.uio.no>
2020-10-14 13:37:26 +0200

    added docstrings for this file

--- assignment5/requesting_urls.py
+++ assignment5/requesting_urls.py
@@ -1,8 +1,27 @@
+"""
+This program will take in a url or parts of a url and get the html for that page. 
+It can also optionally take in parameters as part of urls. It will also optionally
+make a output file of the html and save it in the requesting_urls folder.  
+"""
 import requests as req
+
 
 resp = req.get("https://en.wikipedia.org/wiki/Studio_Ghibli")
 
 def get_html(url, params=None, output=None):
+    """
+    This function takes in a url and get the html for that page. It can get parameters
+    too that will be parts of the url. It can also get in a filename and save the 
+    html in a text file
+
+    parameters:
+        url (String): Will be a url to a web page.
+        params (Dict): A dictionary of some parameters to be concatinated to the url.
+        output (String): A string that will be the filename of the output file.
+
+    returns:
+        resp (<class 'requests.models.Response'>): A html file
+    """
     if url == None:
         print("No url found")
         exit(0)
@@ -28,3 +47,4 @@ resp2 = get_html(url2, None, "output2")
 resp3 = get_html(url3, None, "output3")
 resp4 = get_html(url4_5, params4, "output4")
 resp5 = get_html(url4_5, params5, "output5")
+print(type(resp1))
